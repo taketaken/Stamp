8bf1fabf0e1321441ee664ddcc5735053a8b9490
Fumiya <tokugawa.home1600@gmail.com>
2015-06-08 19:33:46 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by Fumiya on 2015/06/08.
+//  Copyright (c) 2015年 Fumiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StampTests/StampTests.swift
@@ -0,0 +1,36 @@
+//
+//  StampTests.swift
+//  StampTests
+//
+//  Created by Fumiya on 2015/06/08.
+//  Copyright (c) 2015年 Fumiya. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class StampTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
